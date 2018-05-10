3c699dfba521c8bc51a8aed1bd9b575bad21507a
samratbitm <samratbitm17@gmail.com>
2018-05-10 07:00:42 +0530

    Working Bot

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 // Create chat connector for communicating with the Bot Framework Service
+// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: "eb3b2005-39e7-4fb1-b987-59e4d3c20dfd",
-    appPassword: "670d059a-12b1-4084-a4e7-e146fa9465cf"
+    appPassword: "ybH6+Sro$i(z+k7_"
 });
 
 // Listen for messages from users 
